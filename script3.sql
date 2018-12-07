bce7113bf1d5567afbc1829a3b20fb13e35975a8
Eduardo Samano <44065386+eduardo-samano-globant@users.noreply.github.com>
2018-12-07 11:38:52 -0600

    Create script3.sql

---
+++ script3.sql
@@ -0,0 +1,3 @@
+INSERT INTO `db_changes`.`new_table` (`column1`,`column2`) VALUES (1, 'test1');
+INSERT INTO `db_changes`.`new_table` (`column1`,`column2`) VALUES (2, 'test2');
+INSERT INTO `db_changes`.`new_table` (`column1`,`column2`) VALUES (3, 'test3');
