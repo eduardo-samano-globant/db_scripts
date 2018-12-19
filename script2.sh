fa5e52afc30b542c05f62be6b598ca10911bf7c1
Eduardo Samano <44065386+eduardo-samano-globant@users.noreply.github.com>
2018-12-19 16:31:31 -0600

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -5,5 +5,5 @@
   --user=$(vault read -field=username data-eng/vivid-master-rw)  \
   --password=$(vault read -field=password data-eng/vivid-master-rw)  \
   --critical-load=Threads_running=200  \
-  --alter "ADD COLUMN columnper VARCHAR(45) NOT NULL DEFAULT '' AFTER column2" \
+  --alter "ADD COLUMN columnper VARCHAR(45) NOT NULL DEFAULT '' AFTER column1" \
   h=10.231.8.25,D=db_changes,t=new_table
