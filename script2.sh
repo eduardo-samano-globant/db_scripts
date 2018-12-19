83b398ebe877c9c9e9df3fe6cd94b0df9ca93510
Eduardo Samano <44065386+eduardo-samano-globant@users.noreply.github.com>
2018-12-19 13:01:04 -0600

    Create script2.sh

---
+++ script2.sh
@@ -0,0 +1,11 @@
+  pt-online-schema-change  \
+  --execute  \
+  --progress=time,30 \
+  --nocheck-replication-filters  \
+  --user=$(vault read -field=username data-eng/vivid-master-rw)  \
+  --password=$(vault read -field=password data-eng/vivid-master-rw)  \
+  --critical-load=Threads_running=200  \
+  --alter "ADD COLUMN columnper VARCHAR(45) NOT NULL DEFAULT '' AFTER column2" \
+  h=10.231.8.25,\
+  D=db_changes,\
+  t=new_table
