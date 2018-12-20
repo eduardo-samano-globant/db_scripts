689b37210d6253ce298cc3e20dde444a54ef55ac
Eduardo Samano <44065386+eduardo-samano-globant@users.noreply.github.com>
2018-12-20 15:29:37 -0600

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -3,7 +3,6 @@
   --progress=time,30 \
   --nocheck-replication-filters  \
   --user=$DB_USERNAME  \
-  --password=$DB_PASSWORD  \
   --critical-load=Threads_running=200  \
   --alter "ADD COLUMN columnper VARCHAR(45) NOT NULL DEFAULT '' AFTER column1" \
   h=10.231.8.25,D=db_changes,t=new_table
