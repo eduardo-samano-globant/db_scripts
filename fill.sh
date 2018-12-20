f55428301b285ff4b3a4ad01db52275df18aff3c
eduardo-samano-globant <hadoop369>
2018-12-20 17:20:26 -0600

    test

--- fill.sh
+++ fill.sh
@@ -1,1 +1,2 @@
-for i in $(seq 1 100000); do mysql -u "$DB_USERNAME" -p"$DB_PASSWORD" -h  10.231.8.25 -D db_changes -s -e "INSERT INTO new_table VALUES (NULL);"; done
+#!/usr/bin/env bash
+for i in $(seq 1 100000); do mysql -u "$DB_USERNAME" -h  10.231.8.25 -D db_changes -s -e "INSERT INTO new_table VALUES (NULL);"; done
