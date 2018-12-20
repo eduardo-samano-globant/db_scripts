a587a734901bd04244fab188023111c1bfddbbcb
Eduardo Samano <44065386+eduardo-samano-globant@users.noreply.github.com>
2018-12-20 13:47:12 -0600

    Update fill.sh

--- fill.sh
+++ fill.sh
@@ -1,1 +1,1 @@
-for i in $(seq 1 10000000); do mysql -u "$DB_USERNAME" -p"$DB_PASSWORD" -h  10.231.8.25 -D db_changes -s -e "INSERT INTO new_table VALUES (NULL);"; done
+for i in $(seq 1 100000); do mysql -u "$DB_USERNAME" -p"$DB_PASSWORD" -h  10.231.8.25 -D db_changes -s -e "INSERT INTO new_table VALUES (NULL);"; done
