5dc2ff988c850efe898f1257d4697e21d663ec37
Eduardo Samano <44065386+eduardo-samano-globant@users.noreply.github.com>
2018-12-20 13:36:09 -0600

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mysql -u "$DB_USERNAME" -p"$DB_PASSWORD" -h 10.231.8.25 -e "CREATE TABLE db_changes.new_table ( \
-  column1 INT NOT NULL, \
+  column1 INT NOT NULL AUTO_INCREMENT, \
   PRIMARY KEY (column1));"
