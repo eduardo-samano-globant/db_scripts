605424137711926f4fc82c273e56cde26fb097c1
Eduardo Samano <44065386+eduardo-samano-globant@users.noreply.github.com>
2018-12-20 13:02:24 -0600

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-mysql -u "$(vault read -field=username data-eng/vivid-master-rw)" -p"$(vault read -field=password data-eng/vivid-master-rw)" -h 10.231.8.25 -e "CREATE TABLE db_changes.new_table ( \
+mysql -u "$DB_USERNAME" -p"$DB_PASSWORD" -h 10.231.8.25 -e "CREATE TABLE db_changes.new_table ( \
   column1 INT NOT NULL, \
   PRIMARY KEY (column1));"
